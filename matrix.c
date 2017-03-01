3b6b6135882ce2aacf3b927be62d79a1f00da73f
oreo21 <reok99@gmail.com>
2017-03-01 01:22:38 -0500

    matrix operations finished

--- matrix.c
+++ matrix.c
@@ -12,6 +12,13 @@ Returns:
 print the matrix
 */
 void print_matrix(struct matrix *m) {
+  int i, j;
+  for (i = 0; i < (*m).rows; i++){
+    for (j = 0; j < (*m).cols; j++){
+      printf("%f ", (*m).m[i][j]);        
+    }
+    printf("\n");
+  }
 }
 
 /*-------------- void ident() --------------
@@ -21,6 +28,13 @@ Returns:
 turns m in to an identity matrix
 */
 void ident(struct matrix *m) {
+  int i, j;
+  for (i = 0; i < (*m).rows; i++){
+    for (j = 0; j < (*m).cols; j++){
+      if (i == j) (*m).m[i][j] = 1.0;
+      else (*m).m[i][j] = 0.0;
+    }
+  }
 }
 
 
@@ -32,6 +46,12 @@ Returns:
 multiply each element of m by x
 */
 void scalar_mult(double x, struct matrix *m) {
+  int i, j;
+  for(i = 0; i < (*m).rows; i++){
+    for (j = 0; j < (*m).rows; j++){
+      (*m).m[i][j] *= x;
+    }
+  }
 }
 
 
@@ -43,8 +63,18 @@ Returns:
 a*b -> b
 */
 void matrix_mult(struct matrix *a, struct matrix *b) {
+  if ((*a).cols != (*b).rows) return;
+  int i, j, k;
+  struct matrix *c;
+  c = new_matrix((*b).rows, (*b).cols);
+  for(i = 0; i < (*b).rows; i++)
+    for(j = 0; j < (*b).cols; j++)
+      for (k = 0; k < (*a).rows; k++)
+	(*c).m[i][j] += (*a).m[i][k] * (*b).m[k][j];
+  //printf("printing product...\n"), print_matrix(c);
+  copy_matrix(c, b);
+  free_matrix(c);
 }
-
 
 /*===============================================
   These Functions do not need to be modified
@@ -132,3 +162,18 @@ void copy_matrix(struct matrix *a, struct matrix *b) {
       b->m[r][c] = a->m[r][c];  
 }
 
+int main(){
+
+  struct matrix *a, *b;
+  a = new_matrix(2, 2), b = new_matrix(2, 2);
+  (*a).m[0][0] = 1.0, (*a).m[0][1] = 2.0;
+  (*a).m[1][0] = 3.0, (*a).m[1][1] = 4.0;
+  (*b).m[0][0] = 1.0, (*b).m[0][1] = 2.0;
+  (*b).m[1][0] = 3.0, (*b).m[1][1] = 4.0;
+  printf("printing matrix a...\n"), print_matrix(a);
+  printf("printing matrix b...\n"), print_matrix(b);
+  matrix_mult(a, b);
+  printf("printing product...\n"), print_matrix(b);
+  
+  return 0;
+}
